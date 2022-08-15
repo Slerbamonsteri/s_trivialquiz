506bdb98c31f3775cbfd17d323e2ddfc5c490b4b
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2022-08-15 19:17:40 +0300

    Adding Configs for easier usage

---
+++ s_trivialquiz/config.lua
@@ -0,0 +1,8 @@
+
+Config = {
+    CommandName = 'testquiz', --Add your wanted command here which should be used to answer in quiz //
+    AlternateMethod = true, --Change this to true if you don't want to use answer box and rather use /answer "answer" -method // 
+    NewQuestion = 15, --How often should new quiz be broadcast to server (in minutes) //
+    payoutMin = 2500, --Self explanatory //
+    payoutMax = 6000, --Self explanatory //
+}
